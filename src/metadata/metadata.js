f3c453858d1e07fdbbac21f334873afe1814130c
rr-breno-aquino <os-breno.aquino@rakuten.com>
2023-10-17 18:00:29 -0300

    adding pt-br metadata

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -7,8 +7,8 @@ module.exports = {
     'Email': '<a href="mailto:breno@brenoaquino.dev">breno@brenoaquino.dev</a>'
   },
   overview: `
-  I am a person who is always looking for innovation and automating everything possible, enabling us to focus on more important tasks. I have experience with projects in the areas of education, streaming and finance. I am always open to discuss about architecture and performance, we always can learn more with a good discussion.
-`,
+  Tenho quase 6 anos de experiência com desenvolvimento iOS. Já fui responsável pela criação (tanto desenvolvimento quanto desenhar a arquitetura/solução) de múltiplas SDKs, incluindo uma white label que teria seu tema customizado pelo aplicativo que iria utilizá-la e outra que era responsável por implementar uma framework de analytics que seria utilizado em outros aplicativos da empresa. Tenho vasta experiência em streaming e já trabalhei na área financeira cuidando de um aplicativo utilizado por milhões de usuários.
+  `,
   skills: [
     ['Swift', '+++++'],
     ['iOS', '+++++'],
@@ -34,10 +34,10 @@ module.exports = {
       period: 'October 2022 - current',
       skills: ['iOS', 'Swift', 'Objective C', 'UIKit', 'View Code', 'SwiftUI', 'Unit Testing', 'XCTest', 'Modules', 'Github Actions', 'Xcode Cloud', 'Fastlane', 'SDK'],
       contents: `
-* Responsible to design and create a custom new SDK for the analytics;
-* Implemented an authentication feature that is modified depending on the user's country;
-* Implemented a privacy module to internationalize the app and follow the laws of each country/state.
-`
+      * Responsável por projetar e criar uma SDK personalizado para o analytics da empresa;
+      * Implementei um recurso de autenticação que é modificado dependendo do país do usuário;
+      * Implementei um módulo de privacidade para internacionalizar o aplicativo e seguir as leis de cada país/estado.
+      `
     },
     {
       company: 'Itaú',
@@ -46,10 +46,10 @@ module.exports = {
       period: 'March 2022 - October 2022',
       skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'XCTest', 'Modules', 'Swift Package Manager', 'Cocoapods', 'Jenkins', 'Fastlane'],
       contents: `
-* Responsible for leading and mentoring a Junior developer, teaching and guiding on iOS development;
-* Implemented several individual modules and distributed using Cocoapods;
-* Responsible for maintaining the entire notification flow, composed of push notifications, deeplinks and a feature that allows the user to keep track of its notifications.
-`
+      * Responsável por liderar e orientar um desenvolvedor Junior, ensinando e orientando no desenvolvimento iOS;
+      * Implementei vários módulos individuais e distribuídos usando Cocoapods;
+      * Responsável por manter todo o fluxo de notificações, composto por push notifications, deeplinks e uma funcionalidade que permite ao usuário acompanhar suas notificações.
+      `
     },
     {
       company: 'Meta',
@@ -58,12 +58,12 @@ module.exports = {
       period: 'December 2020 - February 2022 (part time)',
       skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Quick', 'Nimble', 'XCTest', 'WebSocket', 'SDK', 'Cocoapods', 'CI/CD', 'Github Actions', 'Scrum'],
       contents: `
-* Worked as a leader creating and defining the base structure project for the client Maple Leaf Sports & Entertainment (MLSE). Defining the priority of functionalities, architectures, APIs and web socket for real-time behavior;
-* Implemented an SDK following Clean and MVVM to provide features like live and VOD streams, chats, real-time stats and other features and distributed using Cocoapods;
-* Implemented a design system and a custom theme to be defined by the host app;
-* Implemented scripts and configured the CI (using Github Action) to automate the build and distribution of the compiled SDK and its documentation;
-* Used Agile and Scrum methodologies, also having weekly meetings with the client and the client backend team.
-`
+      * Atuei como líder criando e definindo o projeto de estrutura base para o cliente Maple Leaf Sports & Entertainment (MLSE). Definindo a prioridade de funcionalidades, arquiteturas utilizadas, métricas, APIs e web socket para comportamento em tempo real;
+      * Implementei uma SDK seguindo Clean e MVVM para fornecer recursos como transmissões ao vivo e VOD, chats, estatísticas em tempo real, mini jogos e outros recursos e distribuído usando Cocoapods;
+      * Implementei um design system e um tema personalizado a ser definido pelo aplicativo host;
+      * Implementei scripts e configurei o CI (usando Github Action) para automatizar a construção e distribuição do SDK compilado e sua documentação;
+      * Utilizei metodologias Agile e Scrum, realizando também reuniões semanais com o cliente e a equipe de backend do cliente.
+      `
     },
     {
       company: 'Claro',
@@ -72,13 +72,13 @@ module.exports = {
       period: 'March 2019 - March 2022',
       skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Quick', 'Nimble', 'XCTest', 'Swift Package Manager', 'CI/CD', 'Fastlane', 'Bitrise', 'SAFe'],
       contents: `
-* Developed one stream project from the beginning and improved another streaming project, called NOW Online. Both of them provided live and VOD contents;
-* Worked as a leader of 2 other iOS developers defining the technologies, architecture and priorities;
-* Helped to define the entire solution and the architecture for the project, always looking for performance;
-* Implemented a feature to list all channels and their programs, to improve the performance and the time to render the screen to the user, I implemented a custom cache algorithm to avoid unnecessary recalculations and binary search to improve the time to find the current program based on the time;
-* Improved the architecture to use MVVM and follow Clean Architecture on the NOW project. This one had about 300,000+ monthly active users;
-* Increased the test coverage applying the dependency injection concept.
-`
+      * Desenvolvi um projeto de stream desde o início e aprimorei outro projeto de stream, chamado NOW Online. Ambos fornecem conteúdos ao vivo e VOD;
+      * Trabalhei como líder de outros 2 desenvolvedores iOS definindo as tecnologias, arquitetura e prioridades;
+      * Ajudei a definir a solução e a arquitetura do projeto, sempre buscando performance;
+      * Implementei um recurso para listar todos os canais e seus programas, para melhorar o desempenho e o tempo de renderização da tela para o usuário, implementei um algoritmo de cache customizado para evitar recálculo desnecessários e busca binária para melhorar o tempo para encontrar o programa atual baseado em na hora atual;
+      * Melhorou a arquitetura para usar MVVM e seguir o Clean Architecture no projeto NOW, além de ter começado a implementar testes unitários. Este aplicativo tinha cerca de 300.000+ usuários ativos mensais;
+      * Aumentei a cobertura de testes aplicando e apliquei o conceito de injeção de dependência.
+      `
     },
     {
       company: 'University of Fortaleza - UNIFOR',
@@ -87,25 +87,13 @@ module.exports = {
       period: 'March 2018 - March 2019',
       skills: ['iOS', 'Swift', 'UIKit', 'Storyboard', 'View Code', 'UI Testing', 'MVC', 'MVVM', 'Tech Lead', 'Ruby on Rails', 'Fastlane'],
       contents: `
-* Worked as a Tech Lead for a team of 10 interns;
-* Responsible for all the university iOS Apps. The main university app had about 24,000+ monthly active users;
-* Taught iOS courses for interns and students;
-* Responsible for teaching about the iOS, git, scrum methodology and also how to design the entire solution, from the UX, priorities, APIs, front end to tests;
-* Refactored the app following MVVM. Among the features, the student's enrollment, the class frequency and the campus map with all the university's landmarks stand out.
-`
+      * Atuei como Tech Lead para uma equipe de 10 estagiários;
+      * Fui responsável por todos os aplicativos iOS da universidade. O principal aplicativo da universidade tinha mais de 24.000 usuários ativos mensais;
+      * Ministrei cursos de iOS para estagiários e estudantes;
+      * Responsável por ensinar sobre a metodologia iOS, git, scrum e também como desenhar toda a solução, desde UX, prioridades, APIs, front end até testes;
+      * Liderei a refatoração do principal aplicativo da universidade seguindo MVVM. Dentre as funcionalidades, se destaca a matrícula do aluno, a frequência realizada pelo professor e o mapa do campus com todos os pontos de referência da universidade.
+      `
     },
-//     {
-//       company: 'University of Fortaleza - UNIFOR',
-//       location: 'Fortaleza - CE, Brazil',
-//       title: 'Intern - iOS Developer',
-//       period: 'July 2017 - March 2018',
-//       skills: ['iOS', 'Swift'],
-//       contents: `
-// * I learned more about iOS working on the main university app, especially working on the chat and the map feature that could guide the users through the university.
-// * Refactored the app following MVVM.
-// * After 9 months I was hired to be responsible for a team composed of 10 interns. My job was to teach them about iOS and software engineering.
-// `
-//     },
   ],
   education: [
     {
@@ -114,43 +102,33 @@ module.exports = {
       course: 'Computing Engineering',
       period: '2015 - 2019',
       contents: `
-* Final Project: Research using recurrent neural networks to predict cryptocurrency trends.
-* Object Orientation class assistant in 2016.
-* Microprocessor class assistant in 2017.
-* Researcher in the area of sensor network security, trying to implement an algorithm to detect attacks.
-`
+      * Trabalho de Conclusão de Curso: Pesquisa usando uma rede neural recorrente para tentar predizer a tendência de criptomoedas.
+      * Monitor da disciplina de Programação Orientada a Objetos em 2016.
+      * Monitor da disciplina de Microprocessor em 2017.
+      * Pesquisa na área de segurança em redes de sensores, tentando implementar um algoritmo para detecção de ataques.
+      `
     }
   ],
   personal_projects: [
     {
-      title: 'iOS Movies',
-      skills: ['iOS', 'Swift', 'SwiftUI', 'Design System', 'MVVM'],
-      contents: 'App integrated with TheMovieDB to be a base project to apply new technologies or create a POC.',
-    },
-    {
       title: 'iOS Cash Handler',
       skills: ['iOS', 'Swift', 'SwiftUI', 'Design System', 'MVVM', 'View Code'],
-      contents: 'This project in development creates an app that uses SwitUI, Clean Architecture, Coordinator and SPM modules.',
+      contents: 'Esse projeto cria um aplicativo para gestão financeira com o objetivo de aplicar conhecimentos em SwiftUI, Coordinator, SPM e Clean Architecture.',
     },
     {
       title: 'iOS Common Module',
       skills: ['iOS', 'Swift', 'UIKit', 'Design System', 'View Code'],
-      contents: 'A set of extensions for views to improve code readability, write and maintenance constraints and allowing easy integration with design systems.',
+      contents: 'Um conjunto de extensões para views para melhorar a legibilidade de código, criação de constraints e permitindo uma fácil integração com um design system.',
     },
     {
       title: 'Home Security System',
       skills: ['Computer Vision', 'Python', 'Flask', 'WebSocket', 'MongoDB', 'Raspberry Pi', 'Computer Vision', 'Machine Learning'],
-      contents: 'System to detect people and objects using IP cameras.'
-    },
-    {
-      title: 'C.E.I. B3 - Crawler',
-      skills: ['Web Scraping', 'NodeJS', 'Python', 'Serverless', 'AWS DynamoDB', 'AWS Lambda'],
-      contents: 'Crawler project to extract data from a Brazilian Stock Market platform and give you an overview about your investments by APIs.'
+      contents: 'Sistema de deteção de pessoas e objetos local utilizando câmera IP. Podendo ser utilizado com alarme ou para automações.'
     },
     {
       title: 'Mobile Stores (App Store e Play Store) - Crawler',
       skills: ['Web Scraping', 'Python', 'Serverless', 'AWS Step Functions', 'AWS Lambda', 'AWS DynamoDB'],
-      contents: 'Crawler to extract reviews from App Store and Play Store.'
+      contents: 'Crawler para extrair reviews da App Store e Play Store e tentar extrair o sentimento (positivo ou negativo) do comentário.'
     }
   ]
-};
+};  
